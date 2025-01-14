0c60146852ea9e5ddec5d8c13a0f8a708cf9ff29
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2025-01-14 11:47:08 -0500

    Slider attempt 1, asset added

--- FindTheRoots/RootCalculatorView.swift
+++ FindTheRoots/RootCalculatorView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct RootCalculatorView: View {
+    
+    // Mark: Stroed properties
+    @State var a: Double = 1
+    
+    //Mark: Computer properties
+
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            
+            VStack {
+                
+                
+                
+                Image("quad")
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack(alignment: .top) {
+                    
+                    
+                    VStack {
+                        Slider(value: $a, in: -100...100, step: 0.1,
+                               minimumValueLabel: Text("-100"),
+                               maximumValueLabel: Text("100"))
+                        Text("a: \(Int(a))")
+                            .padding(.bottom)
+                        
+                        }
+                    VStack {
+                    }
+                    VStack {
+                        
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Find the Roots")
     }
 }
 
